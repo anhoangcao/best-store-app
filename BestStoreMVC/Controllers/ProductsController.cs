c42661df3d3d27d6c70735e0349364956df70e7a
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2024-06-19 17:27:15 +0700

    Add project files.

---
+++ BestStoreMVC/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using BestStoreMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestStoreMVC.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = context.Products.OrderByDescending(p => p.Id).ToList(); // Sắp xếp id theo thứ tự giảm dần
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
