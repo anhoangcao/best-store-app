c64e178611cec6126b8f27efc678a60c5190d78c
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2024-06-24 00:35:22 +0700

    up pagination

--- BestStoreMVC/Controllers/HomeController.cs
+++ BestStoreMVC/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using BestStoreMVC.Models;
+using BestStoreMVC.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,16 +7,17 @@ namespace BestStoreMVC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ApplicationDbContext context)
         {
-            _logger = logger;
+            this.context = context;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var products = context.Products.OrderByDescending(p => p.Id).Take(4).ToList();
+            return View(products);
         }
 
         public IActionResult Privacy()
---
+++ BestStoreMVC/Controllers/StoreController.cs
@@ -0,0 +1,41 @@
+﻿using BestStoreMVC.Models;
+using BestStoreMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BestStoreMVC.Controllers
+{
+    public class StoreController : Controller
+    {
+        private readonly ApplicationDbContext context;
+        private readonly int pageSize = 8;
+
+        public StoreController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index(int pageIndex)
+        {
+            IQueryable<Product> query = context.Products;
+
+            query = query.OrderByDescending(p => p.Id);
+
+            // pagination functionality
+            if (pageIndex < 1)
+            {
+                pageIndex = 1;
+            }
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / pageSize);
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+
+            var products = query.ToList();
+            
+            ViewBag.Products = products;
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
+
+            return View();
+        }
+    }
+}
