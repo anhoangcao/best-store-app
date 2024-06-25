b7ae0fa4e9db0110dffcee75b850fa892eb1a3ea
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2024-06-25 23:23:18 +0700

    up edit profile

--- BestStoreMVC/Controllers/AccountController.cs
+++ BestStoreMVC/Controllers/AccountController.cs
@@ -123,9 +123,64 @@ namespace BestStoreMVC.Controllers
         }
 
         [Authorize]
-        public IActionResult Profile()
+        public async Task<IActionResult> Profile()
         {
-            return View();
+            var appUser = await userManager.GetUserAsync(User);
+            if (appUser == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var profileDto = new ProfileDto()
+            {
+                FirstName = appUser.FirstName,
+                LastName = appUser.LastName,
+                Email = appUser.Email ?? "",
+                PhoneNumber = appUser.PhoneNumber,
+                Address = appUser.Address,
+            };
+
+            return View(profileDto);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Profile(ProfileDto profileDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.ErrorMessage = "Please fill all the required fields with valid values";
+                return View(profileDto);
+            }
+
+            // Get the current user
+            var appUser = await userManager.GetUserAsync(User);
+            if (appUser == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Update the user profile
+            appUser.FirstName = profileDto.FirstName;
+            appUser.LastName = profileDto.LastName;
+            appUser.UserName = profileDto.Email;
+            appUser.Email = profileDto.Email;
+            appUser.PhoneNumber = profileDto.PhoneNumber;
+            appUser.Address = profileDto.Address;
+
+            var result = await userManager.UpdateAsync(appUser);
+
+            if (result.Succeeded)
+            {
+                ViewBag.SuccessMessage = "Profile updated successfully";
+            } 
+            else
+            {
+                ViewBag.ErrorMessage = "Unable to update the profile: " + result.Errors.First().Description;
+            }
+
+
+            return View(profileDto);
         }
 
         public IActionResult AccessDenied()
