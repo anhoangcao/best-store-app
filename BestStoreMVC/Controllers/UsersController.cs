144173343ef4063198eadf6a9918483964a7f2d0
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2024-06-27 15:28:53 +0700

    up user details

--- BestStoreMVC/Controllers/UsersController.cs
+++ BestStoreMVC/Controllers/UsersController.cs
@@ -11,16 +11,52 @@ namespace BestStoreMVC.Controllers
     {
         private readonly UserManager<ApplicationUser> userManager;
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly int pageSize = 5;
 
         public UsersController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
         {
             this.userManager = userManager;
             this.roleManager = roleManager;
         }
-        public IActionResult Index()
+        public IActionResult Index(int? pageIndex)
         {
-            var users = userManager.Users.OrderByDescending(u => u.CreatedAt).ToList();
+            IQueryable<ApplicationUser> query = userManager.Users.OrderByDescending(u => u.CreatedAt);
+            
+            // pagination functionality
+            if (pageIndex == null || pageIndex < 1)
+            {
+                pageIndex = 1;
+            }
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / pageSize);
+            query = query.Skip(((int)pageIndex - 1) * pageSize).Take(pageSize);           
+            
+            var users = query.ToList();
+
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
+            
             return View(users);
+        }
+
+        public async Task<IActionResult> Details(string? id)
+        {
+            if (id == null)
+            {
+                return RedirectToAction("Index", "Users");
+            }
+
+            var appUser = await userManager.FindByIdAsync(id);
+
+            if (appUser == null)
+            {
+                return RedirectToAction("Index", "Users");
+            }
+
+            ViewBag.Roles = await userManager.GetRolesAsync(appUser);
+
+            return View(appUser);
         }
     }
 }
