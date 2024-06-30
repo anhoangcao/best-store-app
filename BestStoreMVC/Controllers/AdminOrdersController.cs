33906e6cddecee3a6ff10d74cebcdc1761967961
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2024-06-30 15:26:58 +0700

    update admin order of pagination

--- BestStoreMVC/Controllers/AdminOrdersController.cs
+++ BestStoreMVC/Controllers/AdminOrdersController.cs
@@ -1,4 +1,5 @@
-﻿using BestStoreMVC.Services;
+﻿using BestStoreMVC.Models;
+using BestStoreMVC.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -10,18 +11,34 @@ namespace BestStoreMVC.Controllers
     public class AdminOrdersController : Controller
     {
         private readonly ApplicationDbContext context;
+        private readonly int pageSize = 5;
 
         public AdminOrdersController(ApplicationDbContext context)
         {
             this.context = context;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int pageIndex)
         {
-            var orders = context.Orders.Include(o => o.Client)
-                .Include(o => o.Items).OrderByDescending(o => o.Id).ToList();
+            IQueryable<Order> query = context.Orders.Include(o => o.Client)
+                .Include(o => o.Items).OrderByDescending(o => o.Id);
+            
+            if (pageIndex <= 0)
+            {
+                pageIndex = 1;
+            }
+
+            decimal count = query.Count();
+            int totalPages = (int)Math.Ceiling(count / pageSize);
+
+            query = query.Skip((pageIndex - 1) * pageSize).Take(pageSize);
+            
+            var orders = query.ToList();
 
             ViewBag.Orders = orders;
+            ViewBag.PageIndex = pageIndex;
+            ViewBag.TotalPages = totalPages;
+
 
             return View();
         }
