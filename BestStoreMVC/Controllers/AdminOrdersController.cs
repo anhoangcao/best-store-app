f04c23d7f8a24af15ae64f6e7766f6961986e709
HoangAn-B2003774 <115530751+HoangAn-B2003774@users.noreply.github.com>
2024-06-30 14:46:59 +0700

    admin orders display orders to admin

---
+++ BestStoreMVC/Controllers/AdminOrdersController.cs
@@ -0,0 +1,29 @@
+﻿using BestStoreMVC.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestStoreMVC.Controllers
+{
+    [Authorize(Roles = "admin")]
+    [Route("/Admin/Order/{action=Index}/{id?}")]
+    public class AdminOrdersController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public AdminOrdersController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var orders = context.Orders.Include(o => o.Client)
+                .Include(o => o.Items).OrderByDescending(o => o.Id).ToList();
+
+            ViewBag.Orders = orders;
+
+            return View();
+        }
+    }
+}
